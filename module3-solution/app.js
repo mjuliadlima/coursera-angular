8b5197f7b46168967636d6ad9405db750a51780a
Maria Julia Lima <mjulia@tecgraf.puc-rio.br>
2017-05-29 14:35:18 -0300

    Correcao dos erros

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -10,16 +10,23 @@ angular.module('NarrowItDownApp', [])
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
   var controller = this;
+
+  // narrowIt
   controller.narrowIt = function() {
-    console.log("narrowIt");
-    var promise = MenuSearchService.getMatchedMenuItems(controller.searchTerm);
-    promise.then(function(response) {
-      controller.items = response;
-    })
-    .catch(function(error) {
-      console.log("Something went wrong", error);
-    });
+    if (controller.searchTerm===undefined || controller.searchTerm.trim()==="") {
+      controller.items = [];
+    } else {
+      var promise = MenuSearchService.getMatchedMenuItems(controller.searchTerm);
+      promise.then(function(response) {
+        controller.items = response;
+      })
+      .catch(function(error) {
+        console.log("Something went wrong", error);
+      });
+    }
   };
+
+  // removeItem
   controller.removeItem = function(index) {
       controller.items.splice(index, 1);
   };
@@ -28,20 +35,19 @@ function NarrowItDownController(MenuSearchService) {
 MenuSearchService.$inject = ['$http', 'ApiBasePath'];
 function MenuSearchService($http, ApiBasePath) {
   var service = this;
+  var matchedItems = [];
   service.getMatchedMenuItems = function (searchTerm) {
     return $http({
-      method: "GET",
-      url: (ApiBasePath + "/menu_items.json")
-    }).then(function (result) {
-      console.log("Filtrar pelo nome ", searchTerm);
-      var items = result.data.menu_items;
-      if (!searchTerm) return items;
-      console.log(items);
-      return items.filter(function (item) {
-        return item.description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
+        method: "GET",
+        url: (ApiBasePath + "/menu_items.json")
+      }).then(function (result) {
+        var items = result.data.menu_items;
+        matchedItems = items.filter(function (item) {
+          return item.description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
+        });
+        return matchedItems;
       });
-    });
-  };
+  }
 }
 
 function FoundItemsDirective() {
